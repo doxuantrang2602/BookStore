d997796e0ae9438ab7a71d966b3452bde8496dde
Do Xuan Trang <xuantrang260203@gmail.com>
2024-06-19 10:42:41 +0700

    fix done Project

--- article/article.js
+++ article/article.js
@@ -41,8 +41,27 @@ window.onscroll = function () {
 };
 
 function smoothScrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
+    const targetPosition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000; 
+    let start = null;
+
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutQuad(progress, startPosition, distance, duration));
+        if (progress < duration) {
+            window.requestAnimationFrame(step);
+        }
+    }
+
+    window.requestAnimationFrame(step);
+}
+
+function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
 }
